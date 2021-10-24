056d25dca5ae01ffaeb3857f5b7852ed3c735f4a
kasun clement <kasun@ceytechsystemsolutions.com>
2021-10-24 18:31:01 +0530

    layout changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -381,9 +381,9 @@ div {
                                 <label for=""><b>Year Range</b></label><br>
                                 <div class='row'>
                                     <div class="col-12">
-									<div class="input-group form-control">
-                                        <input type="text" id="year_min" name="year_min" class="yearpicker col-6" placeholder="MIN" autocomplete="off">
-                                        <input type="text" id="year_max" name="year_max" class="yearpicker col-6" placeholder="MAX" autocomplete="off">
+									<div class="input-group">
+                                        <input type="text" id="year_min" name="year_min" class="yearpicker col-6 form-control" placeholder="MIN" autocomplete="off">
+                                        <input type="text" id="year_max" name="year_max" class="yearpicker col-6 form-control" placeholder="MAX" autocomplete="off">
 										</div>
                                     </div>
                                 </div>
