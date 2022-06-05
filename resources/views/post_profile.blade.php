59ec76a22ba70b5f1ad3adb12054a67b145f0646
kasun <kasun@ceytechsystemsolutions.com>
2022-06-05 17:28:43 +0530

    partial works

--- resources/views/post_profile.blade.php
+++ resources/views/post_profile.blade.php
@@ -16,67 +16,71 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-12 col-md-7">
-                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                                <ol class="carousel-indicators">
-                                    @if($post_data->main_image != null)
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                    @endif
-                                    @if($post_data->image_1 != null)
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                    @endif
-                                    @if($post_data->image_2 != null)
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                                    @endif
-                                    @if($post_data->image_3 != null)
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                                    @endif
-                                    @if($post_data->image_4 != null)
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-                                    @endif
-                                    @if($post_data->image_5 != null)
-                                    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
-                                    @endif
-                                </ol>
-                                <div class="carousel-inner">
-                                    @if($post_data->main_image != null)
-                                    <div class="carousel-item active">
-                                        <img class="d-block w-100 img-responsive" src="{{asset("".$post_data->main_image)}}" style="height: 100%;" alt="First slide">
+                            <div class="card card-light">
+                                <div class="card-body">
+                                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                        <ol class="carousel-indicators">
+                                            @if($post_data->main_image != null)
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                            @endif
+                                            @if($post_data->image_1 != null)
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                            @endif
+                                            @if($post_data->image_2 != null)
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                            @endif
+                                            @if($post_data->image_3 != null)
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                                            @endif
+                                            @if($post_data->image_4 != null)
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                                            @endif
+                                            @if($post_data->image_5 != null)
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                                            @endif
+                                        </ol>
+                                        <div class="carousel-inner">
+                                            @if($post_data->main_image != null)
+                                            <div class="carousel-item active">
+                                                <img class="d-block w-100 img-responsive" src="{{asset("".$post_data->main_image)}}" style="height: 100%;" alt="First slide">
+                                            </div>
+                                            @endif
+                                            @if($post_data->image_1 != null)
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 img-responsive" src="{{asset("".$post_data->image_1)}}" style="height: 100%;" alt="Second slide">
+                                            </div>
+                                            @endif
+                                            @if($post_data->image_2 != null)
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 img-responsive" src="{{asset("".$post_data->image_2)}}" style="height: 100%;" alt="Third slide">
+                                            </div>
+                                            @endif
+                                            @if($post_data->image_3 != null)
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 img-responsive" src="{{asset("".$post_data->image_3)}}" style="height: 100%;" alt="Fourth slide">
+                                            </div>
+                                            @endif
+                                            @if($post_data->image_4 != null)
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 img-responsive" src="{{asset("".$post_data->image_4)}}" style="height: 100%;" alt="Fifth slide">
+                                            </div>
+                                            @endif
+                                            @if($post_data->image_5 != null)
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100 img-responsive" src="{{asset("".$post_data->image_5)}}" style="height: 100%;" alt="Sixth slide">
+                                            </div>
+                                            @endif
+                                        </div>
+                                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
                                     </div>
-                                    @endif
-                                    @if($post_data->image_1 != null)
-                                    <div class="carousel-item">
-                                        <img class="d-block w-100 img-responsive" src="{{asset("".$post_data->image_1)}}" style="height: 100%;" alt="Second slide">
-                                    </div>
-                                    @endif
-                                    @if($post_data->image_2 != null)
-                                    <div class="carousel-item">
-                                        <img class="d-block w-100 img-responsive" src="{{asset("".$post_data->image_2)}}" style="height: 100%;" alt="Third slide">
-                                    </div>
-                                    @endif
-                                    @if($post_data->image_3 != null)
-                                    <div class="carousel-item">
-                                        <img class="d-block w-100 img-responsive" src="{{asset("".$post_data->image_3)}}" style="height: 100%;" alt="Fourth slide">
-                                    </div>
-                                    @endif
-                                    @if($post_data->image_4 != null)
-                                    <div class="carousel-item">
-                                        <img class="d-block w-100 img-responsive" src="{{asset("".$post_data->image_4)}}" style="height: 100%;" alt="Fifth slide">
-                                    </div>
-                                    @endif
-                                    @if($post_data->image_5 != null)
-                                    <div class="carousel-item">
-                                        <img class="d-block w-100 img-responsive" src="{{asset("".$post_data->image_5)}}" style="height: 100%;" alt="Sixth slide">
-                                    </div>
-                                    @endif
                                 </div>
-                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span class="sr-only">Next</span>
-                                </a>
                             </div>
                         </div>
                         <div class="col-12 col-md-5">
