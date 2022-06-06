dd4c9994cd5f85b0b4fb6eff79836fc9b156bb92
kasun <kasun@ceytechsystemsolutions.com>
2022-06-06 15:38:44 +0530

    partial works

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -431,6 +431,7 @@
                     html += '<span style="font-size: 14px" class="text-dark"><b>Price: </b>' + row.price + ' </span><br>';
                     html += '<span style="font-size: 14px" class="text-dark"><b>Location: </b>' + row.location + ' </span><br>';
                     html += '<span style="font-size: 14px" class="text-dark"><b>Condition: </b>' + row.condition + ' </span>';
+                    html += '<span style="font-size: 14px" class="text-dark"><b>Millage: </b>' + row.millage + ' </span>';
                     html += '</div>';
                     html += '<div class="col-1">';
                     html += '<div class="ribbon-wrapper ribbon-sm">';
