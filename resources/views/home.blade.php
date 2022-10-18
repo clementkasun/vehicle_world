6cc37acfe108cf033450d1758608c99297a78b0c
kasun <kasun@ceytechsystemsolutions.com>
2022-10-18 12:39:17 +0530

    partialy completed the ui update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -780,7 +780,7 @@
                     html += '<div class="row">';
                     html += '<div class="col-3 col-md-2">';
                     html += '<div class="portfolio-wrap text-center">';
-                    html += "<img src='/public/" + row.main_image + "' style='height: 8em; width: 55%;' alt='main_img'/>";
+                    html += "<img src='/public/" + row.main_image + "' style='height: 8em; width: 70%;' alt='main_img'/>";
                     html += '</div>';
                     html += '</div>';
                     html += '<div class="col-8 col-md-9">';
