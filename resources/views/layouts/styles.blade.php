9b6450ccd92bb1122f55b3960bb8f61d56f04a29
kasun <kasun@ceytechsystemsolutions.com>
2022-10-19 17:16:34 +0530

    partialy completed the ui update

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -93,7 +93,6 @@
 
 <!-- Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Barlow:200,300,300i,400,400i,500,500i,600,700,800" rel="stylesheet">
-<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,600italic,700,700italic,800' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 
