17f15dcdccbae8c0ad7bed19024b1b728021535d
kasun <kasun@ceytechsystemsolutions.com>
2022-11-18 23:05:55 +0530

    backend and frontend optimizations

--- resources/views/registration/post_registration.blade.php
+++ resources/views/registration/post_registration.blade.php
@@ -403,7 +403,7 @@
             <div class="row">
                 <div class="card card-light col-12">
                     <div class="card-body row">
-                        <div class="col-6">
+                        <div class="col-12 col-md-6">
                             <div class="form-group">
                                 <label for="main_image">Main Image</label>
                                 <div>
@@ -423,7 +423,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-6">
+                        <div class="col-6 col-md-6">
                             <div class="form-group">
                                 <label for="image_three">Image 3</label>
                                 <div>
